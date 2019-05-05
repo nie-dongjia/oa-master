5760bcca320df47e57afb00c988f49743beb923a
NieDongjia <327803131@qq.com>
2019-05-05 21:55:12 +0800

    ## 连接池最优解

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# oa - master 
+## oa - master 
 activity 流程设计 for  OA or CRM system 
 
 
@@ -9,3 +9,10 @@ https://blog.csdn.net/a67474506/article/details/38266129
 --利用分布式服务器框架，封装远程接口，整合多节点工作流引擎，支持大并发服务
 
 -- 业务可以在web 应用，远程调用服务。封装的工作流服务。。。。
+
+
+##  流程单据业务类都可以使用activiti使用处理
+
+
+
+  
--- doc/DB_Activiti.sql
+++ doc/DB_Activiti.sql
@@ -6,3 +6,40 @@ SELECT COUNT(*) TABLES, table_schema FROM information_schema.TABLES   WHERE tabl
 
 
 
+
+SELECT TRANSACTION;
+
+SHOW DATABASES;
+
+SHOW COLUMNS FROM act_evt_log; 	
+
+SHOW COLUMNS FROM `act_id_info`
+
+
+SHOW STATUS;
+
+SHOW GRANTS;
+
+SELECT * FROM t_sys_userinfo LIMIT 2
+SELECT COUNT(1) FROM t_sys_userinfo 
+
+
+
+SHOW VARIABLES LIKE 'slow%';
+SET GLOBAL slow_query_log = ON;
+SELECT VERSION();
+SELECT * FROM `act_hi_taskinst` ORDER BY ID_ DESC LIMIT 2
+-- 
+SELECT * FROM `act_hi_taskinst` ORDER BY ID_ DESC LIMIT 4,2 
+-- 
+
+DESCRIBE act_hi_taskinst
+
+
+
+
+
+
+
+
+
